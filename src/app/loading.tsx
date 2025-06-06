5970f676487d081b94e65679d2f1fb8d63ccdf21
Thiago D'Alessandro <89609966+Thdalessa@users.noreply.github.com>
2025-06-06 11:11:54 -0300

    [ADD] loading component

---
+++ src/app/loading.tsx
@@ -0,0 +1,7 @@
+import Loader from "@/components/shared/Loader/Loader";
+
+const loading = () => {
+  return <Loader />;
+};
+
+export default loading;
---
+++ src/components/shared/Loader/Loader.tsx
@@ -0,0 +1,7 @@
+import { FC } from "react";
+
+const Loader: FC = () => {
+  return <div>Cargando...</div>;
+};
+
+export default Loader;
