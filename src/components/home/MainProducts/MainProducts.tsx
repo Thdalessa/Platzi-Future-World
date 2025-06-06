fe2a060e0ef9aca0f241bbdf53c98b18cf38c980
Thiago D'Alessandro <89609966+Thdalessa@users.noreply.github.com>
2025-06-06 11:12:40 -0300

    [UPDATE] Description and MainProducts components

--- src/components/home/MainProducts/MainProducts.tsx
+++ src/components/home/MainProducts/MainProducts.tsx
@@ -1,9 +1,48 @@
-const MainProducts = () => {
-  return (
-    <section>
-      <h2>MainProducts</h2>
-    </section>
-  )
-}
+import Image from "next/image";
+import styles from "./MainProducts.module.scss";
+const getProducts = async () => {
+  try {
+    const res = await fetch(
+      `${process.env.SHOPIFY_HOSTNAME}/admin/api/2025-04/products.json`,
+      {
+        headers: new Headers({
+          "X-Shopify-Access-Token": process.env.SHOPIFY_API_KEY || "",
+        }),
+      }
+    );
+    const { products } = await res.json();
+    return products;
+  } catch (err) {
+    console.log(err);
+  }
+};
 
-export default MainProducts
+const MainProducts = async () => {
+  const products = await getProducts();
+  console.log(products, process.env.SHOPIFY_HOSTNAME);
+  return (
+    <section className={styles.MainProducts}>
+      <h2>New products released!</h2>
+      <div className={styles.MainProducts__grid}>
+        {products?.map((product: any) => {
+          const imageSrc = product.images[0].src;
+          return (
+            <article key={product.id}>
+              <p>{product.title}</p>
+              <div>
+                <Image
+                  src={imageSrc}
+                  alt={product.title}
+                  fill
+                  loading="eager"
+                />
+              </div>
+            </article>
+          );
+        })}
+      </div>
+    </section>
+  );
+};
+
+export default MainProducts;
